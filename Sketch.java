87238d777b094fb19eedfb28ce3ccf7d1787ead8
RomuelC <romuel.cuevas23@ycdsbk12.ca>
2022-01-18 21:42:20 -0500

    Halfway

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,56 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+	float[] circleY = new float[25];
+
+  int num = 50;
+  int[] x = new int[num];
+  int[] y = new int[num];
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
     size(400, 400);
+
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+    }
+          for (int p = num-1; p > 0; p--) {
+            x[p] = x[p-1];
+            y[p] = y[p-1];
+          }
+  
+          x[0] = mouseX;
+          y[0] = mouseY;    
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+ 
   public void setup() {
     background(210, 255, 173);
+
+    size(100, 100);
+    noStroke();
+    fill(255, 102);
+    background(0);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    background(50);
+
+    for (int i = 0; i < circleY.length; i++) {
+      float circleX = width * i / circleY.length;
+      ellipse(circleX, circleY[i], 25, 25);
   
-  // define other methods down here.
+      circleY[i] ++;
+  
+      if (circleY[i] > height) {
+        circleY[i] = 0;
+
+    for (int p = 0; p < num; p++) {
+      ellipse(x[p], y[p], p/ (float) 2.0, p/ (float) 2.0);          
+          }
+      }
+          
+  }
+}
 }
